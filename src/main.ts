7649a7d13f9db2ada523a76cb4c9d29fe035630c
Devy Salomon <devysalomon@gmail.com>
2025-07-28 00:51:38 -0500

    actualizaciones

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3001'], // Orígenes permitidos
+    origin: [process.env.CORS_ORIGIN], // Orígenes permitidos
     methods: 'GET,PUT,POST,PATCH,DELETE', // Métodos permitidos
     allowedHeaders: 'Content-Type, Accept', // Encabezados permitidos
     credentials: true, // Habilitar el envío de cookies y cabeceras de autenticación
